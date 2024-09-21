3ad4e4f8739a635fc6ba17aecf7b1dbbb36e80ad
mazerti <titouan.mazier@grenoble-inp.org>
2024-09-21 17:38:26 +0200

    Create rough layout
    The behavior of the preview needs to be updated to something nicer.

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -2,6 +2,15 @@ import { PageProps } from "~/lib/page-props";
 import { getUser } from "~/server/user";
 import { notFound, redirect } from "next/navigation";
 import { routeLogin, routeProject } from "~/app/routes";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "~/components/ui/resizable";
+import { ScrollArea } from "~/components/ui/scroll-area";
+import { cn } from "~/lib/utils";
+import { Card, CardContent } from "~/components/ui/card";
+import { Separator } from "~/components/ui/separator";
 
 interface ProjectPageProps extends PageProps {
   params: { projectId: string };
@@ -21,9 +30,37 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
 
   return (
     <main className="flex h-screen w-full items-center justify-center bg-background px-4">
-      <ul>
-        <li>List of things</li>
-      </ul>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel defaultSize={150}>
+          <ScrollArea
+            className={cn(
+              "inline-flex",
+              "items-center",
+              "size-full",
+              "bg-orange-500",
+            )}
+          >
+            <Card className={cn("self-center", "w-full", "aspect-[21/29.7]")}>
+              <CardContent>
+                <ul>
+                  <li>List of things</li>
+                </ul>
+              </CardContent>
+            </Card>
+          </ScrollArea>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={50}>
+          <ScrollArea className="size-full">
+            {[...Array(25)].map((e, i) => (
+              <>
+                <div key={e}>Add Component here</div>
+                <Separator className="my-2" />
+              </>
+            ))}
+          </ScrollArea>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </main>
   );
 }
