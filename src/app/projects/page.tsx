02941ab000ca525318801da18db035d3d556dcb9
mazerti <titouan.mazier@grenoble-inp.org>
2024-09-23 17:29:32 +0200

    Write a rough display for project list

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,9 @@
-import { getUser } from "~/server/user";
+import { getProjects, getUser } from "~/server/user";
 import { redirect } from "next/navigation";
 import { routeLogin, routeProjects } from "~/app/routes";
+import { cn } from "~/lib/utils";
+import { Separator } from "@radix-ui/react-menubar";
+import { Label } from "~/components/ui/label";
 
 export default async function ProjectsPage() {
   const user = await getUser();
@@ -9,11 +12,23 @@ export default async function ProjectsPage() {
     return redirect(routeLogin({ redirectTo: routeProjects() }));
   }
 
+  const projects = await getProjects();
+  if (!projects || projects.length == 0) {
+    return (
+      <main className="flex h-screen w-full items-center justify-center bg-background px-4">
+        <Label>No projects created yet</Label>
+      </main>
+    );
+  }
+
   return (
     <main className="flex h-screen w-full items-center justify-center bg-background px-4">
-      <ul>
-        <li>List of things</li>
-      </ul>
+      {projects.map((project, i) => (
+        <>
+          {i > 0 ? <Separator /> : <></>}
+          <Label>{project.name}</Label>
+        </>
+      ))}
     </main>
   );
 }
