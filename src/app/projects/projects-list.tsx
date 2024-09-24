69fed79283041c31ac0f2e8e88fa80e3d9a0454c
mazerti <titouan.mazier@grenoble-inp.org>
2024-09-24 13:11:59 +0200

    Link to project editor (layout is todo)

--- src/app/projects/projects-list.tsx
+++ src/app/projects/projects-list.tsx
@@ -6,31 +6,46 @@ import {
   CardContent,
   CardFooter,
   CardHeader,
+  CardTitle,
 } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
 import { createDummyProject } from "./actions";
+import Link from "next/link";
+import { routeProject } from "../routes";
+import { cn } from "~/lib/utils";
 
 export interface ProjectsListProps {
   projects: { id: string; name: string }[];
 }
-export default function ProjectsList({projects}: ProjectsListProps) {
+export default function ProjectsList({ projects }: ProjectsListProps) {
   return (
     <Card>
       <CardHeader>
-        <Label>Your projects:</Label>
+        <CardTitle className={cn("text-2xl")}>Your projects:</CardTitle>
       </CardHeader>
       <CardContent>
         {projects.map((project, i) => (
-          <div key={project.id}>
-            <Label>{project.name}</Label>
+          <div key={i}>
+            <Link
+              href={routeProject(project.id)}
+              className={cn(
+                "w-full",
+                "px-6",
+                "rounded-l",
+                "hover:bg-accent",
+                "hover:text-accent-foreground",
+              )}
+            >
+              <Label>{project.name}</Label>
+            </Link>
           </div>
         ))}
       </CardContent>
       <CardFooter>
         <Button
-        onClick={async () => {
-          await createDummyProject();
-        }}
+          onClick={async () => {
+            await createDummyProject();
+          }}
         >
           Create one more
         </Button>
