3a7b268987c2a4a0725640fb89a706ca5c3077bf
mazerti <titouan.mazier@grenoble-inp.org>
2024-10-01 12:03:44 +0200

    Make text fields autosize in editor

--- src/components/cv/elements/simple-text/simple-text.edit.tsx
+++ src/components/cv/elements/simple-text/simple-text.edit.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Textarea } from "~/components/ui/textarea";
 import { SimpleTextElement } from "~/components/cv/elements/simple-text/simple-text.schema";
 import { useDispatch } from "~/components/cv/context";
 import { setText } from "./simple-text.state";
+import TextAreaAutosize from "react-textarea-autosize"
 
 export interface SimpleTextEditProps {
   element: SimpleTextElement;
@@ -13,8 +13,8 @@ const SimpleTextEdit = ({ element }: SimpleTextEditProps) => {
 
   return (
     <div className="relative p-2">
-      <Textarea
-        className="w-full"
+      <TextAreaAutosize
+        className="w-full resize-none"
         value={element.data.text}
         onChange={(event) => {
           dispatch(setText(element.id, event.target.value));
