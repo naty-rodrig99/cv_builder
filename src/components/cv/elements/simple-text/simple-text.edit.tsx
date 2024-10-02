7426ddd9aea6814eb4831641232b2d1cedcdbf46
Natalia Rodriguez Arias <165667280+naty-rodrig99@users.noreply.github.com>
2024-10-02 15:44:20 +0200

    Added text editor from Lexical framework

--- src/components/cv/elements/simple-text/simple-text.edit.tsx
+++ src/components/cv/elements/simple-text/simple-text.edit.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import './text-edit-styles.css';
 import { Textarea } from "~/components/ui/textarea";
 import { SimpleTextElement } from "~/components/cv/elements/simple-text/simple-text.schema";
 import { useDispatch } from "~/components/cv/context";
+
+import {LexicalComposer} from '@lexical/react/LexicalComposer';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
+
+import ToolbarTextEdit from './toolbar-text.edit';
 
 export interface SimpleTextEditProps {
   element: SimpleTextElement;
@@ -9,6 +18,23 @@ export interface SimpleTextEditProps {
 
 const SimpleTextEdit = ({ element }: SimpleTextEditProps) => {
   const dispatch = useDispatch();
+
+  const placeholder = 'Enter some rich text...';
+
+  const editorConfig = {
+    editable: true,
+    namespace: 'Rich Text Editor',
+    nodes: [],
+    onError(error: Error) {
+      throw error;
+    },
+  };
+
+  //on change function
+  function textEdited(){
+    //const editorState = editor.getEditorState();
+    //const json = editorState.toJSON();
+  }
 
   return (
     <div className="relative p-2">
@@ -19,6 +45,26 @@ const SimpleTextEdit = ({ element }: SimpleTextEditProps) => {
           // TODO: dispatch(updateTextElementText(event.target.value));
         }}
       />
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className="editor-container">
+        <ToolbarTextEdit />
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                className="editor-input"
+                aria-placeholder={placeholder}
+                placeholder={
+                  <div className="editor-placeholder">{placeholder}</div>
+                }
+              />
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+        </div>
+      </div>
+    </LexicalComposer>
     </div>
   );
 };
