c28c717099e061698fcde18f0de2ef45fc89e02b
mazerti <titouan.mazier@grenoble-inp.org>
2024-10-04 15:07:38 +0200

    format edition toolbar

--- src/components/cv/elements/EditorTools.tsx
+++ src/components/cv/elements/EditorTools.tsx
@@ -38,10 +38,12 @@ export function EditionTools<T extends string>({
 
   return (
     <Menubar
-      className={cn((isSelected && "absolute -top-10 z-40") || "hidden")}
+      className={cn(
+        (isSelected && "absolute -top-10 z-40") || "hidden",
+      )}
     >
       <MenubarMenu>
-        <MenubarLabel className="flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground">
+        <MenubarLabel className="flex cursor-default select-none items-center whitespace-nowrap rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground">
           {element.type}
         </MenubarLabel>
       </MenubarMenu>
