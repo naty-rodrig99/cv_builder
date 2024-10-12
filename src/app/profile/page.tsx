78a6eb53735db5cc6e67ed1160845d2a22d2514c
Lennard Scheibel <contact@lennardscheibel.de>
2024-10-12 22:28:31 +0200

    Fix saving profile data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 // src/app/profile/page.tsx
 
-import { fetchUser } from "./actions";
+import { fetchUser, saveProfile, type UserProfile } from "./actions";
 import ProfileForm from "../../components/cv/elements/profile-element/profile-form";
 
 export default async function ProfilePage() {
@@ -12,7 +12,17 @@ export default async function ProfilePage() {
   return (
     <main className="flex h-screen w-full items-center justify-center bg-background px-4">
       <div>
-        <ProfileForm profileData={result.value} />
+        <ProfileForm
+          profileData={result.value}
+          saveProfileDataAction={async (profile: UserProfile) => {
+            "use server";
+            const result = await saveProfile(profile);
+            if (!result.ok) {
+              throw new Error("Failed to update profile data.");
+            }
+            return result.value;
+          }}
+        />
       </div>
     </main>
   );
