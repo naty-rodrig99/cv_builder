31c2cfa071097d493edf79932acfaa93af3619f8
Lennard Scheibel <contact@lennardscheibel.de>
2024-10-15 21:00:06 +0200

    Fix left panel text alignment

--- src/components/cv/cv-editor.tsx
+++ src/components/cv/cv-editor.tsx
@@ -53,7 +53,7 @@ const Draggable = ({ type, children }: DraggableProps) => {
   });
 
   return (
-    <button ref={setNodeRef} {...listeners} {...attributes}>
+    <button className="w-full" ref={setNodeRef} {...listeners} {...attributes}>
       {children}
     </button>
   );
@@ -293,7 +293,7 @@ const CvEditor = ({ cv, saveAction, exportAction }: CvEditorProps) => {
               <ZoomButtons />
             </ResizablePanel>
           </ResizablePanelGroup>
-          <DragOverlay>
+          <DragOverlay className="text-center">
             {activeElementType ? (
               <DynamicElementPreview elementType={activeElementType} />
             ) : null}
